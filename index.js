60379af7175f459db749cfe728e2f29e74f95944
dyerfire9 <canadianraza@yahoo.com>
2022-08-18 00:03:34 -0400

    All files initialized

---
+++ index.js
@@ -0,0 +1,67 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+    createFile(formattext(myLeads), 'My Leads.txt', 'text/plain')
+
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+function createFile(text, name, type) {
+    let link = document.getElementById("a");
+    let file = new Blob([text], { type: type });
+    link.href = URL.createObjectURL(file);
+    link.download = name;
+}
+
+function formattext(data) {
+    let text = ``
+    for (let i = 0; i < data.length; i++) {
+        text += data[i] + '\n'
+    }
+    return text
+}
+
+//let button = document.getElementById("btn")
+//button.addEventListener("click", createFile(formattext(myLeads), 'myfilename.txt', 'text/plain'))
